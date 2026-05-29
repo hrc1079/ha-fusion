a6bd296fa7ae3a6cc88e06899464cafd3a7105dd
hrc1079 <heath.cox@me.com>
2026-05-28 17:01:57 -0700

    Update Index.svelte

--- src/lib/Settings/Index.svelte
+++ src/lib/Settings/Index.svelte
@@ -82,6 +82,7 @@
 			const plex_target_client_id = (form.plex_target_client_id as string) || undefined;
 			const plex_server_machine_id = (form.plex_server_machine_id as string) || undefined;
 			const plex_android_tv_entity = (form.plex_android_tv_entity as string) || undefined;
+			const plex_cast_entity = (form.plex_cast_entity as string) || undefined;
 			if (
 				plex_enabled ||
 				plex_url ||
@@ -89,7 +90,8 @@
 				plex_account_token ||
 				plex_target_client_id ||
 				plex_server_machine_id ||
-				plex_android_tv_entity
+				plex_android_tv_entity ||
+				plex_cast_entity
 			) {
 				const plex: any = {};
 				if (plex_enabled) plex.enabled = true;
@@ -99,6 +101,7 @@
 				if (plex_target_client_id) plex.target_client_id = plex_target_client_id;
 				if (plex_server_machine_id) plex.server_machine_id = plex_server_machine_id;
 				if (plex_android_tv_entity) plex.android_tv_entity = plex_android_tv_entity;
+				if (plex_cast_entity) plex.cast_entity = plex_cast_entity;
 				json.plex = plex;
 				$configuration.plex = plex;
 			}
