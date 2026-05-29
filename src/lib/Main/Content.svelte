8c5bd8029391057a2e9ddbe56847904e88e3d0f7
hrc1079 <heath.cox@me.com>
2026-05-28 23:31:22 -0700

    Add files via upload

--- src/lib/Main/Content.svelte
+++ src/lib/Main/Content.svelte
@@ -9,11 +9,12 @@
 	import CustomPanel from '$lib/Main/CustomPanel.svelte';
 	import PlayerGroup from '$lib/Main/PlayerGroup.svelte';
 	import PlexHubRow from '$lib/Main/PlexHubRow.svelte';
+	import TidalRow from '$lib/Main/TidalRow.svelte';
 
 	export let item: any;
 	export let sectionName: string | undefined = undefined;
 
-	const large = ['conditional_media', 'picture_elements', 'camera', 'plex_hub_row'];
+	const large = ['conditional_media', 'picture_elements', 'camera', 'plex_hub_row', 'tidal_row'];
 </script>
 
 {#if item?.[SHADOW_ITEM_MARKER_PROPERTY_NAME] && large.includes(item?.type)}
@@ -38,6 +39,8 @@
 	<PlayerGroup sel={item} />
 {:else if item?.type === 'plex_hub_row'}
 	<PlexHubRow sel={item} />
+{:else if item?.type === 'tidal_row'}
+	<TidalRow sel={item} />
 {:else}
 	<!-- if types are changed internally, don't break ui -->
 	<Configure sel={{ id: item?.id }} />
